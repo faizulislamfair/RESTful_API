675c50ceedaf6941ba107fa80da6148cdc7232e7
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-04-13 21:28:57 +0600

    parsing request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const mongoose = require('mongoose')
 
 const app = express()
 
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
 mongoose.connect('mongodb://localhost:27017/RestAPI', {
     useNewUrlParser: true,
     useUnifiedTopology: true
@@ -11,8 +14,21 @@ mongoose.connect('mongodb://localhost:27017/RestAPI', {
   console.log('Mongodb connected.....')
 })
 
+app.all('/test', (req, res) => {
+    // console.log(req.query);
+    // console.log(req.query.name);
+    // res.send(req.query);
+    // console.log(req.params);
+    // res.send(req.params);
+    console.log(req.body)
+    res.send(req.body)
+})
+
 const ProductRoute = require('./Routes/Product.route')
 app.use('/products', ProductRoute)
+
+
+// 404 handler and pass to error handler
 
 app.use((req, res, next) => {
    const err = new Error("Not found")
