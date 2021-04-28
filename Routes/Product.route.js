891368d4072a1c5b19345634b147eb6af3d9e36f
S.M. Faizul Islam Fair <faizulislamfair@gmail.com>
2021-04-28 21:35:51 +0600

    MVC controller pattern

--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -1,120 +1,28 @@
 const express = require('express')
 const router = express.Router()
-const createError = require('http-errors')
-const mongoose = require('mongoose')
 
-const Product = require('../Models/Product.model')
+
+const ProductController = require('../Controllers/Product.Controller')
 
 // Get a list of all products
-router.get('/', async (req, res, next) => {
-    try {
-        const results = await Product.find({}, {__v: 0})
-        // const results = await Product.find({}, { name: 1, price: 1, _id: 0})
-        // const results = await Product.find({price: 699}, {});
-        res.send(results);
-    } catch (error) {
-        console.log(error.message)
-    }
-})
+router.get('/', ProductController.getAllProducts)
 
 
 // Create a new product
-router.post('/', async (req, res, next) => {
-
-    try {
-        const product = new Product(req.body)
-        const result = await product.save()
-        res.send(result)
-    } catch (error) {
-        console.log(error.message);
-        if(error.name === 'ValidationError') {
-            next(createError(422, error.message))
-            return
-        }
-        next(error)
-    }
-
-
-    // const product = new Product({
-    //     name: req.body.name,
-    //     price: req.body.price
-    // })
-    // product.save()
-    // .then(result => {
-    //     console.log(result);
-    //     res.send(result);
-    // })
-    // .catch(err => {
-    //     console.log(err.message);
-    // })
-})
+router.post('/', ProductController.createNewProduct)
 
 
 // Get a product by id
-router.get('/:id', async (req, res, next) => {
-    const id = req.params.id
-    try {
-        const product = await Product.findById(id)
-        // const product = await Product.findOne({ _id: id })
-        if (!product) {
-            throw createError(404, "Product does not exist.")
-        }
-        res.send(product)
-    } catch (error) {
-        console.log(error.message)
-        if(error instanceof mongoose.CastError){
-           next(createError(400, "Invalid Product id"))
-           return
-        }
-        next(error)
-    }
-})
+router.get('/:id', ProductController.findProductById)
 
 
 // Update a product by id
 
-router.patch('/:id', async (req, res, next) => {
-    try {
-      const id = req.params.id
-      const updates = req.body
-      const options = {new: true}
-      
-      const result = await Product.findByIdAndUpdate(id, updates, options)
-      if(!result) {
-          throw createError(404, "Product does not exist")
-      }
-      res.send(result)
-
-    } catch (error) {
-      console.log(error.message)
-      if (error instanceof mongoose.CastError) {
-          return(createError(400, "Invalid Product Id"))
-      }
-
-      next(error)
-    }
-})
+router.patch('/:id', ProductController.updateAProduct)
 
 
 // Delete a product by id
 
-router.delete('/:id', async (req, res, next) => {
-     const id = req.params.id
-     try {
-         const result = await Product.findByIdAndDelete(id)
-         console.log(result)
-         if (!result) {
-            throw createError(404, 'Product does not exist.')
-        }
-         res.send(result)
-        } catch (error) {
-         console.log(error.message)
-         if(error instanceof mongoose.CastError){
-            next(createError(400, "Invalid Product id"))
-            return
-         }
-         next(error)
-     }
-})
+router.delete('/:id', ProductController.deleteAProduct)
 
 module.exports = router;
--- app.js
+++ app.js
@@ -17,42 +17,33 @@ mongoose
   console.log('Mongodb connected.....')
 })
 
-app.all('/test', (req, res) => {
-    // console.log(req.query);
-    // console.log(req.query.name);
-    // res.send(req.query);
-    // console.log(req.params);
-    // res.send(req.params);
-    console.log(req.body)
-    res.send(req.body)
-})
+const ProductRoute = require('./Routes/Product.route');
+app.use('/products', ProductRoute);
 
-const ProductRoute = require('./Routes/Product.route')
-app.use('/products', ProductRoute)
-
-
-// 404 handler and pass to error handler
-
+//404 handler and pass to error handler
 app.use((req, res, next) => {
-//    const err = new Error("Not found")
-//    err.status = 404
-//    next(err)
-  next(createError(404, "Not Found"))
-})
+  /*
+  const err = new Error('Not found');
+  err.status = 404;
+  next(err);
+  */
+  // You can use the above code if your not using the http-errors module
+  next(createError(404, 'Not found'));
+});
 
-// Error Handler
-
+//Error handler
 app.use((err, req, res, next) => {
-   res.status(err.status || 500)
-   res.send({
-       error: {
-           status: err.status || 500,
-           message: err.message
-       }
-   })
-})
+  res.status(err.status || 500);
+  res.send({
+    error: {
+      status: err.status || 500,
+      message: err.message
+    }
+  });
+});
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Server started on port 3000.....')
-})
+app.listen(PORT, () => {
+  console.log('Server started on port ' + PORT + '...');
+});
